2e726e8c64d002e2f2b6d93d0d1d5af010d4aac5
Fabricio Orrala <orrala@celmedia.com>
2014-09-17 11:06:50 -0500

    sh para iniciar

---
+++ sistema/maquinaIniciar.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/debian/Desktop/220V
+/usr/bin/python2 /home/debian/Desktop/220V/machine-test.py
+#/usr/bin/python2 /home/debian/Desktop/220V/machine-sinsocial.py
+#/usr/bin/python2 /home/debian/Desktop/220V/machine.py
